b4ac724af72208f9704715f1bae624ac99ad733c
Inga355 <albi1303@gmail.com>
2025-01-12 12:55:45 +0100

    fixed show()

--- products.py
+++ products.py
@@ -15,8 +15,10 @@ class Product:
         else:
             self.active = True
 
+
     def get_quantity(self):
         return self.quantity
+
 
     def set_quantity(self, quantity):
         self.quantity = quantity
@@ -25,24 +27,27 @@ class Product:
         else:
             return self.quantity
 
+
     def is_active(self) -> bool:
         return self.active
+
 
     def activate(self):
         self.active = True
 
+
     def deactivate(self):
         self.active = False
 
+
     def show(self):
-        print(f"----------------------")
-        print(f"Product: {self.name}")
-        print(f"Price: {self.price}")
-        print(f"Quantity: {self.quantity}")
+        return f"Product: {self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
 
     def buy(self, quantity):
         if self.quantity < quantity:
             raise ValueError(f"We don't have enough {self.name}. Only {self.quantity} left!")
         new_quantity = self.quantity - quantity
         return self.set_quantity(new_quantity)
+
 
