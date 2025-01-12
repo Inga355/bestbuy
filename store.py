63003a1bd8cfa71e17abc164ca6b6ad2cf414d7a
Inga355 <albi1303@gmail.com>
2025-01-12 12:54:21 +0100

    deleted test code, fixed get_total_quantity()

--- store.py
+++ store.py
@@ -5,14 +5,18 @@ class Store:
     def __init__(self, product_list):
         self.list_of_products = product_list
 
+
     def add_product(self, product):
         return self.list_of_products.append(product)
+
 
     def remove_product(self, product):
         return self.list_of_products.pop(product)
 
+
     def get_total_quantity(self) -> int:
-        return len(self.list_of_products)
+        return sum(item.get_quantity() for item in self.list_of_products)
+
 
     def get_all_products(self) -> list:
         active_products = []
@@ -21,16 +25,3 @@ class Store:
                 active_products.append(item)
         return active_products
 
-
-
-
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=0),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250),
-               ]
-
-store = Store(product_list)
-products = store.get_all_products()
-print(store.get_total_quantity())
-print(store.get_all_products())
-#print(store.order([(products[0], 1), (products[1], 2)]))
